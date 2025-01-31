ebc0b8c1f156cf14c4d5ec8428f6e2e3fc1d38d9
Lisa <pilipchchlisa@gmail.com>
2025-01-31 20:30:37 +0300

    Initial commit

---
+++ src/main/java/com/example/webflux_rest/WebfluxRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.webflux_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxRestApplication.class, args);
+	}
+
+}
